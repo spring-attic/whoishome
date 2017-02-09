4681bd16de7d9b0aecfaea498c2cc1df37c517ce
Chris Sterling <csterling@pivotal.io>
2017-02-09 12:05:27 -0800

    Added circuit breaker dashboard service to create as dependency before deploying apps in BAT script.

--- scripts/deploy_gradle.bat
+++ scripts/deploy_gradle.bat
@@ -4,11 +4,18 @@ cf create-service p-service-registry standard service-registry > nul
 :check
   cf service service-registry | find "succeeded" > nul
   if errorlevel 1 goto :check
-  echo Service Registry created. Pushing applications.
-  pushd homeowner
-  cf push -p build/libs/homeowner-0.0.1-SNAPSHOT.jar
-  popd
-  pushd visitor
-  cf push -p build/libs/visitor-0.0.1-SNAPSHOT.jar
-  popd
-  echo Done!
+  echo Service Registry created.
+  echo Creating Circuit Breaker Dashboard...
+  cf create-service p-circuit-breaker-dashboard standard circuit-breaker > nul
+  :check
+    cf service circuit-breaker | find "succeeded" > nul
+    if errorlevel 1 goto :check
+    echo Circuit Breaker Dashboard created.
+    echo Pushing applications.
+    pushd homeowner
+    cf push -p build/libs/homeowner-0.0.1-SNAPSHOT.jar
+    popd
+    pushd visitor
+    cf push -p build/libs/visitor-0.0.1-SNAPSHOT.jar
+    popd
+    echo Done!
