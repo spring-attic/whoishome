9001634a0ea1dadd90ac9bd03258a76a21c983f7
Chris Sterling <csterling@pivotal.io>
2017-12-12 10:20:42 -0800

    Removed unnecessary @Bean import. Changed name of visitor application in its application.yml configuration to visitor since it was registering as homeowner causing issues with service calls. Added STS 4 cache to git ignore list.

--- visitor/src/main/java/visitor/VisitorApplication.java
+++ visitor/src/main/java/visitor/VisitorApplication.java
@@ -1,7 +1,6 @@
 package visitor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
