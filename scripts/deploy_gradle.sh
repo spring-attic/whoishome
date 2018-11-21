a6f57521c3a8f89a0dde02e335abf1cf7cdd2403
Alexandre Roman <aroman@pivotal.io>
2018-11-21 17:43:58 +0000

    Add support for already created services

--- scripts/deploy_gradle.sh
+++ scripts/deploy_gradle.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 echo -n "Creating Service Registry..."
 {
   cf create-service p-service-registry standard service-registry
 } &> /dev/null
-until [ `cf service service-registry | grep -c "succeeded"` -eq 1  ]
+until [ `cf service service-registry | grep -c "succeeded"` -ge 1  ]
 do
   echo -n "."
 done
@@ -11,7 +12,7 @@ echo -n "Creating Circuit Breaker Dashboard..."
 {
   cf create-service p-circuit-breaker-dashboard standard circuit-breaker
 } &> /dev/null
-until [ `cf service circuit-breaker | grep -c "succeeded"` -eq 1  ]
+until [ `cf service circuit-breaker | grep -c "succeeded"` -ge 1  ]
 do
   echo -n "."
 done
